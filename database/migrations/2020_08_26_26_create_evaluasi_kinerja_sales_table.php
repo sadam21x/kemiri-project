b89b2363d0af5829debe7eac38a947c84bfdf024
ayunindyarista <50320923+ayunindyarista@users.noreply.github.com>
2020-09-01 00:30:54 +0700

    Update 2020_08_26_26_create_evaluasi_kinerja_sales_table.php

--- database/migrations/2020_08_26_26_create_evaluasi_kinerja_sales_table.php
+++ database/migrations/2020_08_26_26_create_evaluasi_kinerja_sales_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateEvaluasiKinerjaSalesTable extends Migration
+class CreateEvaluasiKinerjaSalesATable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateEvaluasiKinerjaSalesTable extends Migration
      */
     public function up()
     {
-        Schema::create('evaluasi_kinerja_sales', function (Blueprint $table) {
-            $table->integer('ID_EVALUASI_KINERJA_SALES', true);
-            $table->integer('ID_PENJUALAN')->index('FK_TERDAPAT99111');
-            $table->date('TGL_EVALUASI_KINERJA_SALES');
-            $table->string('EVALUASI_SALES_1', 50);
-            $table->string('EVALUASI_SALES_2', 50);
+        Schema::create('evaluasi_kinerja_sales_a', function (Blueprint $table) {
+            $table->integer('ID_EVALUASI_KINERJA_SALES_A', true);
+            $table->integer('ID_MANAJER_MARKETING')->index('FK_MELAKUKAN1');
+            $table->integer('ID_SALES_A')->index('FK_MEMILIKI123');
+            $table->date('TGL_EVALUASI_KINERJA_SALES_A');
+            $table->string('EVALUASI_SALES_A', 300);
         });
     }
 
@@ -29,6 +29,6 @@ class CreateEvaluasiKinerjaSalesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('evaluasi_kinerja_sales');
+        Schema::dropIfExists('evaluasi_kinerja_sales_a');
     }
 }
