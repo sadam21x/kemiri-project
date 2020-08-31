2c4700e3faf8bcf1264402d50d23b586e1709d19
ayunindyarista <50320923+ayunindyarista@users.noreply.github.com>
2020-09-01 00:11:12 +0700

    Update 2020_08_26_24_create_hasil_product_table.php

--- database/migrations/2020_08_26_24_create_hasil_product_table.php
+++ database/migrations/2020_08_26_24_create_hasil_product_table.php
@@ -16,7 +16,7 @@ class CreateHasilProductTable extends Migration
         Schema::create('hasil_product', function (Blueprint $table) {
             $table->integer('KODE_HASIL_PRODUCT', true);
             $table->integer('KODE_PRODUKSI')->index('FK_TERDAPAT999');
-            $table->string('NAMA_PRODUCT', 50);
+            $table->integer('KODE_PRODUCT')->index('FK_TERDAPAT93312');
             $table->float('HASIL_BAGUS_PCS', 10, 0)->nullable();
             $table->float('HASIL_RUSAK_PCS', 10, 0)->nullable();
         });
