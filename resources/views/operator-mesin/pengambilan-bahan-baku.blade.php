3782c9ebceb99ad6d3421c887d332a0d3bad15eb
sadam21x <sadam21x@gmail.com>
2020-09-01 22:23:50 +0700

    revisi pengambilan bahan baku

--- resources/views/operator-mesin/pengambilan-bahan-baku.blade.php
+++ resources/views/operator-mesin/pengambilan-bahan-baku.blade.php
@@ -39,14 +39,10 @@
                         <td>PGB00001</td>
                         <td>01/08/2020 - 10:30</td>
                         <td>Rudi Antara</td>
-                        <td colspan="2">
+                        <td>
                             <button class="btn btn-sm btn-linkedin" data-toggle="modal" data-target="#modal-detail-pengambilan-bahan-baku">
                                 <i class="fas fa-info-circle mr-1"></i>
                                 DETAIL
-                            </button>
-                            <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modal-edit-pengambilan-bahan-baku">
-                                <i class="fas fa-edit"></i>
-                                EDIT
                             </button>
                         </td>
                     </tr>
@@ -54,14 +50,10 @@
                         <td>PGB00002</td>
                         <td>01/08/2020 - 06:15</td>
                         <td>Rama Suastika</td>
-                        <td colspan="2">
+                        <td>
                             <button class="btn btn-sm btn-linkedin" data-toggle="modal" data-target="#modal-detail-pengambilan-bahan-baku">
                                 <i class="fas fa-info-circle mr-1"></i>
                                 DETAIL
-                            </button>
-                            <button class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modal-edit-pengambilan-bahan-baku">
-                                <i class="fas fa-edit"></i>
-                                EDIT
                             </button>
                         </td>
                     </tr>
@@ -83,7 +75,7 @@
                     <i class="fas fa-times-circle text-danger"></i>
                 </button>
             </div>
-            <div class="modal-body">
+            <div class="modal-body modal-body-pengambilan-bahan-baku">
                 <form action="" method="POST">
                     @csrf
 
@@ -91,36 +83,10 @@
                     <input type="hidden" name="" id="" value="">
 
                     <div class="form-group">
-                        <label for="" class="col-form-label">Supplier Bahan Baku</label>
-                        <select class="form-control select-component" id="" name="">
-                            <option>Pilih supplier . . </option>
-                            <option value="UD. Pertama Makmur">UD. Pertama Makmur</option>
-                            <option value="Toko Jaya Sakthi">Toko Jaya Sakthi</option>
-                            <option value="Bapak Zainuri">Bapak Zainuri</option>
-                            <option value="Bapak Santoso">Bapak Santoso</option>
-                            <option value="UD. Dewata Indah">UD. Dewata Indah</option>
-                            <option value="Ibu Nur Aminah">Ibu Nur Aminah</option>
-                            <option value="Himasi">Himasi</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="" class="col-form-label">Bahan Baku</label>
-                        <select class="form-control select-component" id="" name="">
-                            <option>Pilih bahan baku . . </option>
-                            <option value="Plastik Bekas">Plastik Bekas</option>
-                            <option value="Plastik Virgin">Plastik Virgin</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="" class="col-form-label">Jumlah (Kg)</label>
-                        <input type="number" name="" id="" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="" class="col-form-label">Jumlah (Karung)</label>
-                        <input type="number" name="" id="" class="form-control">
+                        <button type="button" class="btn btn-sm rounded btn-twitter tambah-bahan-baku-btn">
+                            <i class="fas fa-plus mr-2"></i>
+                            BAHAN BAKU
+                        </button>
                     </div>
 
                     <div class="form-group">
@@ -141,37 +107,20 @@
                         </select>
                     </div>
 
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-google" data-dismiss="modal">BATAL</button>
-                        <button type="submit" class="btn btn-linkedin">SIMPAN</button>
-                    </div>
-
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-{{-- End of Input Pengambilan Bahan Baku Modal --}}
-
-{{-- Start Edit Pengambilan Bahan Baku Modal --}}
-<div class="modal fade" id="modal-edit-pengambilan-bahan-baku" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
-        <div class="modal-content">
-            <div class="modal-header bg-secondary">
-                <h5 class="modal-title">Edit Pengambilan Bahan Baku</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <i class="fas fa-times-circle text-danger"></i>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="" method="POST">
-                    @csrf
-
-                    {{-- Hidden id pengambilan bahan baku untuk update data --}}
-                    <input type="hidden" name="">
+                    <hr>
 
                     <div class="form-group">
-                        <label for="" class="col-form-label">Supplier Bahan Baku</label>
+                        <label class="col-form-label">Bahan Baku</label>
+                        <select class="form-control select-component" id="" name="">
+                            <option>Pilih bahan baku . . </option>
+                            <option value="Plastik Bekas">Plastik Bekas</option>
+                            <option value="Plastik Virgin">Plastik Virgin</option>
+                            <option value="Pewarna">Pewarna</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-form-label">Supplier Bahan Baku</label>
                         <select class="form-control select-component" id="" name="">
                             <option>Pilih supplier . . </option>
                             <option value="UD. Pertama Makmur">UD. Pertama Makmur</option>
@@ -185,45 +134,18 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="" class="col-form-label">Bahan Baku</label>
-                        <select class="form-control select-component" id="" name="">
-                            <option>Pilih bahan baku . . </option>
-                            <option value="Plastik Bekas">Plastik Bekas</option>
-                            <option value="Plastik Virgin">Plastik Virgin</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="" class="col-form-label">Jumlah (Kg)</label>
+                        <label class="col-form-label">Jumlah Bahan Baku (Kg)</label>
                         <input type="number" name="" id="" class="form-control">
                     </div>
 
                     <div class="form-group">
-                        <label for="" class="col-form-label">Jumlah (Karung)</label>
+                        <label class="col-form-label">Jumlah Bahan Baku (Karung)</label>
                         <input type="number" name="" id="" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="" class="col-form-label">Barang Produksi</label>
-                        <select class="form-control select-component" id="" name="">
-                            <option>Pilih barang yang akan diproduksi . . </option>
-                            <option value="Tutup Galon Tipe A">Tutup Galon Tipe A</option>
-                            <option value="Tutup Galon Tipe B">Tutup Galon Tipe B</option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="" class="col-form-label">Mesin yang digunakan</label>
-                        <select class="form-control select-component" id="" name="">
-                            <option>Pilih mesin . . </option>
-                            <option value="Mesin A">Mesin A</option>
-                            <option value="Mesin B">Mesin B</option>
-                        </select>
                     </div>
 
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-google" data-dismiss="modal">BATAL</button>
-                        <button type="submit" class="btn btn-linkedin">SIMPAN</button>
+                        <button type="button" class="btn btn-sm btn-google" data-dismiss="modal">BATAL</button>
+                        <button type="submit" class="btn btn-sm btn-linkedin">SIMPAN</button>
                     </div>
 
                 </form>
@@ -263,21 +185,6 @@
                     </div>
 
                     <div class="my-3">
-                        <h5>Supplier Bahan Baku</h5>
-                        <h6>UD. Permata Langit</h6>
-                    </div>
-
-                    <div class="my-3">
-                        <h5>Bahan Baku</h5>
-                        <h6>Plastik Virgin</h6>
-                    </div>
-
-                    <div class="my-3">
-                        <h5>Jumlah</h5>
-                        <h6>110 Kg - 3 Karung</h6>
-                    </div>
-
-                    <div class="my-3">
                         <h5>Barang yang diproduksi</h5>
                         <h6>Tutup Galon Tipe B</h6>
                     </div>
@@ -286,6 +193,36 @@
                         <h5>Mesin</h5>
                         <h6>Mesin A211</h6>
                     </div>
+
+                    <div class="my-3">
+                        <h5>Bahan Baku 1</h5>
+                        <h6>Plastik Virgin</h6>
+                    </div>
+
+                    <div class="my-3">
+                        <h5>Supplier Bahan Baku 1</h5>
+                        <h6>UD. Permata Langit</h6>
+                    </div>
+
+                    <div class="my-3">
+                        <h5>Jumlah Bahan Baku 1</h5>
+                        <h6>110 Kg - 3 Karung</h6>
+                    </div> 
+
+                    <div class="my-3">
+                        <h5>Bahan Baku 2</h5>
+                        <h6>Pewarna Tekstil</h6>
+                    </div>
+
+                    <div class="my-3">
+                        <h5>Supplier Bahan Baku 2</h5>
+                        <h6>UD. Permata Langit</h6>
+                    </div>
+
+                    <div class="my-3">
+                        <h5>Jumlah Bahan Baku 2</h5>
+                        <h6>110 Kg - 3 Karung</h6>
+                    </div>              
 
                 </div>
 
